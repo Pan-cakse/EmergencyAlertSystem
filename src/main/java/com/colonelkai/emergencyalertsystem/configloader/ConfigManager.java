a488400c00e105fd7af5773f20ea7f8c0d87f09b
ColonelKai <kayraacar@hotmail.co.jp>
2022-11-14 15:49:21 +0300

    MOSEEEEEEEEE

--- src/main/java/com/colonelkai/emergencyalertsystem/configloader/ConfigManager.java
+++ src/main/java/com/colonelkai/emergencyalertsystem/configloader/ConfigManager.java
@@ -25,31 +25,31 @@ public class ConfigManager {
         File configFile = new File(EmergencyAlertSystem.getPlugin().getDataFolder(), "config.yml");
 
         if(!configFile.exists()) { // if config does not exist, thou shall create
-            if(!configFile.getParentFile().mkdir()) {
-                EmergencyAlertSystem.getPlugin().getLogger().warning("Failed to mkdir default config's parent directory.");
-                throw new RuntimeException("Failed to mkdir default config's parent directory.");
+            if(!configFile.getParentFile().exists()) {
+                if (!configFile.getParentFile().mkdir()) {
+                    EmergencyAlertSystem.getPlugin().getLogger().warning("Failed to mkdir default config's parent directory.");
+                    throw new RuntimeException("Failed to mkdir default config's parent directory.");
+                }
             }
 
             InputStream defaultConfigInputStream = EmergencyAlertSystem.getPlugin().getResource("config.yml");
 
             try {
                 if (null == defaultConfigInputStream) {
-                    throw new AssertionError();
+                    throw new RuntimeException("Cannot find config.yml located inside the plugin. Has this been modified?");
                 }
                 Files.copy(
                         defaultConfigInputStream,
                         configFile.toPath(),
                         StandardCopyOption.REPLACE_EXISTING);
             } catch (IOException e) {
-                EmergencyAlertSystem.getPlugin().getLogger().warning("Failed to create default config.");
-                throw new RuntimeException(e);
+                throw new RuntimeException("Failed to create default config.", e);
             }
 
             try {
                 defaultConfigInputStream.close();
             } catch (IOException e) {
-                EmergencyAlertSystem.getPlugin().getLogger().warning("Failed to close default config InputStream.");
-                throw new RuntimeException(e);
+                throw new RuntimeException("Failed to close default config InputStream.", e);
             }
 
         }
@@ -59,13 +59,11 @@ public class ConfigManager {
     }
 
     public void getAllEASTypesFromConfig() {
-        // Set<EASType> easTypeSet = new HashSet<>();
-
         Set<String> keys = this.getConfig().getKeys(false);
 
         EmergencyAlertSystem.getPlugin().getLogger().info("Attempting to load: " + keys);
 
-        Collection<EASType> easTypeSet = keys.parallelStream().map(k -> new EASType(
+        this.setEasTypeSet(keys.parallelStream().map(k -> new EASType(
                 k,
                 this.getConfig().getString(k+".permission"),
                 this.getConfig().getString(k+".sound"),
@@ -74,13 +72,11 @@ public class ConfigManager {
                 this.getConfig().getInt(k+".volume"),
                 this.getConfig().getInt(k+".pitch"),
                 this.getConfig().getInt(k+".sound-length")
-        )).collect(Collectors.toSet());
+        )).collect(Collectors.toSet()));
 
         EmergencyAlertSystem.getPlugin().getLogger().info("Loaded: " +
-                        easTypeSet.parallelStream().map(EASType::getName).toList()
-                );
-
-        this.setEasTypeSet(easTypeSet);
+                  this.getEasTypeSet().parallelStream().map(EASType::getName).toList()
+        );
 
     }
 
